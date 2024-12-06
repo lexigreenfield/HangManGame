8cfec48695487409e9b8364442b39408ea0196d2
lexigreenfield <alexisgreenfield@gmail.com>
2024-12-05 21:57:05 -0800

    adding more classes

---
+++ Player.java
@@ -0,0 +1,19 @@
+public class Player {
+
+    public Player(String name) {
+        name = "lexi";
+    }
+
+    public String getName() {
+        return "hello";
+    }
+
+    // public Points getPoints() {
+
+    // }
+
+    public boolean theirTurn() {
+        return true;
+    }
+
+}
--- Turn.java
+++ Turn.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
 public class Turn {
+    private Player player1;
+    private Player player2;
+
+    private void switchTurn() {
+
+    }
+
+    private Player getCurrentPlayer() {
+        return player1;
+    }
+
+    public void play() {
+
+    }
 
 }
